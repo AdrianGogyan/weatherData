2b0f953789d533938b9260c2d7259d2901e0b37f
AdrianGogyan <gogyanadrian@gmail.com>
2024-07-30 19:51:48 +0200

    Update fetchingApi.js
    switched http to https

--- fetchingApi.js
+++ fetchingApi.js
@@ -1,5 +1,5 @@
 export function constructApi(weatherApikey, weatherApi_lang, weatherApi_location){
-    return `http://api.weatherapi.com/v1/current.json?key=${weatherApikey}&q=${weatherApi_location}&lang=${weatherApi_lang}`;
+    return `https://api.weatherapi.com/v1/current.json?key=${weatherApikey}&q=${weatherApi_location}&lang=${weatherApi_lang}`;
 }
 
 export async function fetchConstructedApi(fullApi){

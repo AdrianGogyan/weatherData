a756c8f5b495245c88a068b59f1f5fb8e6f9b999
AdrianGogyan <gogyanadrian@gmail.com>
2024-08-05 23:28:33 +0200

    update on script and design from the hourly grid

--- main.js
+++ main.js
@@ -12,6 +12,10 @@ const maincondition = document.querySelector(".main-condition");
 const mainstuff = document.querySelector(".main-stuff");
 const appBody = document.getElementById("app");
 
+const hourContainerHeader = document.querySelector(".hourContainer-header");
+const imageOfCondition = document.querySelector(".hourContainer-hours__now__icon");
+const hourContainerHoursNowTemp = document.querySelector(".hourContainer-hours__now__temp");
+
 // document.addEventListener("DOMContentLoaded", () => {
 //     appBody.style.background = `no-repeat center url(${bPic})`;
 // });
@@ -20,11 +24,16 @@ const weatherapi = constructApi(weatherApi_key,weatherApi_lang, weatherApi_locat
 document.addEventListener("DOMContentLoaded", async ()=> {
     try{
         const data = await fetchConstructedApi(weatherapi);
+        console.log(data);
+        hourContainerHeader.innerText =`Heute ist ` + data.current.condition.text + `. Wind: ` + data.current.wind_kph +`km/h.`;
+        imageOfCondition.src = data.current.condition.icon;
         maincity.innerText = data.location.name;
         maintemp.innerText = data.current.temp_c;
+        hourContainerHoursNowTemp.innerText = data.current.temp_c; 
         maincondition.innerText = data.current.condition.text;
         mainstuff.innerText = data.location.lat + " " + data.location.lon;
     } catch(error) {
         console.error(error);
     }
-})
+
+});
